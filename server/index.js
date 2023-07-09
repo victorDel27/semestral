0120ce52b4bab66b2db75f925460400ba0ddc82c
Shakirs Franco <kirsshacofran@gmail.com>
2023-07-08 23:34:43 -0500

    Ya hace conexion papis

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.get("/api", (req, res) => {
-    res.json({ message: "Hola desde el servidor!" });
+    res.json({ message: "Hola desde el servidor PUTITOS" });
 });
 
 app.listen(PORT, () => {
